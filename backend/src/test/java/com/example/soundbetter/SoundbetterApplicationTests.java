192227bc3249547c7b892cb5c0b1968a490fdc52
Thibault <thibault@Thibaults-MacBook-Air.local>
2024-03-19 15:02:21 +0100

    first_commit

---
+++ backend/src/test/java/com/example/soundbetter/SoundbetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soundbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundbetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ database/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "user" (
+    id INT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    user_mail VARCHAR(100) NOT NULL,
+    
+);
